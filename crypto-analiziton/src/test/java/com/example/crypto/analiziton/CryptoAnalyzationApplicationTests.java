178f6ca42e76cfe832614c8456971d540b8c8594
Filvik <filvik@bk.ru>
2024-04-15 14:02:04 +0200

    init

---
+++ crypto-analiziton/src/test/java/com/example/crypto/analiziton/CryptoAnalyzationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crypto.analiziton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAnalyzationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
